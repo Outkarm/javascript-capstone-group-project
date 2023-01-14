24863778582604f2034f3a09270289461cf71c00
Outkarm <jkpordje@gmail.com>
2023-01-14 15:19:57 +0000

    fix linters

--- src/modules/displayMeals.js
+++ src/modules/displayMeals.js
@@ -1,46 +1,46 @@
-import commentSec from "./addcomment.js";
+import commentSec from './addcomment.js';
 
 const showMeal = async (container) => {
   const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s="
+    'https://www.themealdb.com/api/json/v1/1/search.php?s=',
   )
     .then((response) => response.json())
     .then((data) => {
       if (data.meals) {
         data.meals.forEach((meal) => {
-          let id = meal.idMeal;
-          const theMeal = document.createElement("div");
+          const id = meal.idMeal;
+          const theMeal = document.createElement('div');
           theMeal.id = meal.idMeal;
-          theMeal.classList.add("the-meal");
+          theMeal.classList.add('the-meal');
           container.append(theMeal);
-          const theMealImg = document.createElement("img");
+          const theMealImg = document.createElement('img');
           theMealImg.id = meal.idMeal;
           theMealImg.src = meal.strMealThumb;
-          theMealImg.alt = "Dish";
-          theMealImg.classList.add("the-meal-img");
+          theMealImg.alt = 'Dish';
+          theMealImg.classList.add('the-meal-img');
           theMeal.append(theMealImg);
-          const theMealName = document.createElement("p");
+          const theMealName = document.createElement('p');
           theMealName.innerText = meal.strMeal;
           theMealName.id = meal.idMeal;
-          theMealName.classList.add("the-meal-name");
+          theMealName.classList.add('the-meal-name');
           theMeal.append(theMealName);
-          const likeCommentCont = document.createElement("div");
-          likeCommentCont.classList.add("like-comment-cont");
+          const likeCommentCont = document.createElement('div');
+          likeCommentCont.classList.add('like-comment-cont');
           theMeal.append(likeCommentCont);
-          const likeCont = document.createElement("div");
-          likeCont.classList.add("like-cont");
+          const likeCont = document.createElement('div');
+          likeCont.classList.add('like-cont');
           likeCommentCont.append(likeCont);
-          const like = document.createElement("i");
-          like.classList.add("fa-regular", "fa-heart", "like");
+          const like = document.createElement('i');
+          like.classList.add('fa-regular', 'fa-heart', 'like');
           likeCont.append(like);
-          const countLike = document.createElement("span");
+          const countLike = document.createElement('span');
           countLike.innerText = 3;
           likeCont.append(countLike);
-          const commentBtn = document.createElement("button");
-          commentBtn.innerText = "Comment";
+          const commentBtn = document.createElement('button');
+          commentBtn.innerText = 'Comment';
           commentBtn.id = meal.idMeal;
           likeCommentCont.append(commentBtn);
-          commentBtn.addEventListener("click", () => {
+          commentBtn.addEventListener('click', () => {
             if (id === commentBtn.id) {
               commentSec(meal);
             }
@@ -48,6 +48,7 @@ const showMeal = async (container) => {
         });
       }
     });
+  return response;
 };
 
 export { showMeal as default };
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: "./src/index.js",
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
 };
