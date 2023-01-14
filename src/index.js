54dad5ea587b1d0f6b6d00ced1b8b4f962fbdef5
Outkarm <jkpordje@gmail.com>
2023-01-14 15:04:23 +0000

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import "./style.css";
-import showMeal from "./modules/displayMeals.js";
-import addcomment from "./modules/addcomment.js";
-import fetchcomment from "./modules/fetchcomment.js";
-import commentCounterFunction from "./modules/commentCounter.js";
-import addlike from "./modules/addlike.js";
-import fetchlike from "./modules/fetchlike.js";
-import itemCounterFunction from "./modules/itemCounter.js";
-import keyfunction from "./modules/api_key.js";
+import './style.css';
+import showMeal from './modules/displayMeals.js';
 
 // CONTAINER FOR MEALS
 
-const mealsContainer = document.querySelector(".meal-info");
+const mealsContainer = document.querySelector('.meal-info');
 showMeal(mealsContainer);
 
 // const id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 99];
